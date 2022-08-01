a6a7a71a6b0dcd8aa2d5e7f228c5050ff891f1a7
ykolarov <kolarov01@protonmail.me>
2022-08-01 15:06:56 +0100

    Initial changes

--- src/main/java/com/example/northwindserver/controllers/MainController.java
+++ src/main/java/com/example/northwindserver/controllers/MainController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SakilaController {
+public class MainController {
     @GetMapping("/welcome")
     public String welcome() {
         return "welcome.html";
