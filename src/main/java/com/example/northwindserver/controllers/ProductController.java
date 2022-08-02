5cb32b67ac5e819a513d5ad3e541c78cf49717d8
wiater88 <wiater88@gmail.com>
2022-08-02 21:50:44 +0100

    product controler update

--- src/main/java/com/example/northwindserver/controllers/ProductController.java
+++ src/main/java/com/example/northwindserver/controllers/ProductController.java
@@ -5,8 +5,7 @@ import com.example.northwindserver.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +31,31 @@ public class ProductController {
         model.addAttribute("products",result);
         model.addAttribute("list",first);
         return "productPage";
+    }
+
+    @PostMapping({"/createProduct","/updateProduct"})
+    public String createNewProduct(@ModelAttribute Product product){
+        repo.save(product);
+        return "redirect:/product/all";
+    }
+
+    @GetMapping("/addNewProduct")
+    public String addNewProduct(Model model){
+       Product product = new Product();
+       model.addAttribute("product",product);
+        return "newProductForm";
+    }
+
+    @GetMapping("/product/edit/{id}")
+    public String updateProduct(@PathVariable int id, Model model){
+        Product productToEdit = repo.getReferenceById(id);
+        model.addAttribute("productToEdit",productToEdit);
+        return "productEditForm";
+    }
+
+    @GetMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable int id){
+        repo.deleteById(id);
+        return "redirect:/product/all";
     }
 }
